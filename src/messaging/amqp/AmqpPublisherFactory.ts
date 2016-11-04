816978c18ef4755b901ee31b1b17e1bd4b38ef42
Karabutin Alex <karabutinalex@gmail.com>
2016-11-05 00:21:52 +0200

    Fixed message publisher.

--- src/messaging/amqp/AmqpPublisherFactory.ts
+++ src/messaging/amqp/AmqpPublisherFactory.ts
@@ -3,6 +3,7 @@ import * as amqplib from "amqplib";
 import { PublisherFactory } from "../../messaging";
 import { Class, Logger } from "../../core";
 import { AmqpPublisher } from "./AmqpPublisher";
+import {ChannelType} from "../annotations/Message";
 
 export class AmqpPublisherFactory extends PublisherFactory {
 
@@ -17,11 +18,28 @@ export class AmqpPublisherFactory extends PublisherFactory {
 
     public createPublisher<T>(messageClass: Class<T>): Promise<AmqpPublisher<T>> {
 
+        let currentChannel: amqplib.Channel;
+
         return this.connection
             .then((connection: amqplib.Connection) => connection.createChannel() as Promise<amqplib.Channel>)
+            .then((channel: amqplib.Channel) => currentChannel = channel)
             .then((channel: amqplib.Channel) => {
-                channel.assertQueue(messageClass["channelName"], { durable: true });
-                return new AmqpPublisher(channel, messageClass);
-            });
+
+                const exchangeType: string = messageClass["channelType"] === ChannelType.Queue ? "direct" : "fanout";
+
+                this.logger.trace(
+                    `channel.assertExchange("%s", "%s", %s)`,
+                    messageClass["channelName"],
+                    exchangeType,
+                    JSON.stringify({ durable: false })
+                );
+
+                return channel.assertExchange(
+                    messageClass["channelName"],
+                    exchangeType,
+                    { durable: false }
+                );
+            })
+            .then(() => new AmqpPublisher(currentChannel, messageClass));
     }
 }
